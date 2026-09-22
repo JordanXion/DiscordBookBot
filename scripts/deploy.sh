0817deb5d22e5b3b885157a48b807affa247eb32
Jordan <jordan@jxrdxn.xyz>
2026-09-21 23:00:13 -0400

    Use system Python for deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ app_dir="${APP_DIR:-/opt/discord-book-bot}"
 service="${SERVICE_NAME:-bookbot.service}"
 
 cd "$app_dir"
-python3.12 -m venv .venv
+python3 -m venv .venv
 .venv/bin/python -m pip install --upgrade pip
 .venv/bin/python -m pip install -r requirements.txt
 sudo systemctl daemon-reload
